df88d8729438cf4557cff070ad5711f6690f3241
Lai <2946997532@qq.com>
2026-09-16 00:10:28 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "timetable-v5";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon.svg", "./cover.jpg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
+
+
